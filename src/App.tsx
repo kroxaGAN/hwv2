831ca2d8ee520ca1b8b5632efeeeb2005ae604c9
Andrei Hutsau <mwplt@mail.ru>
2022-05-19 09:10:51 +0300

    add tasks for each Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
+export type taskType={
+    id:number,titleTask:string,isDone:boolean
+}
+
 function App() {
+    const tasks1=[
+        {id:1,titleTask:'HTML',isDone:false},
+        {id:2,titleTask:'CSS',isDone:false},
+        {id:3,titleTask:'REact',isDone:true},
+    ]
+    const tasks2=[
+        {id:1,titleTask:'222HTML',isDone:true},
+        {id:2,titleTask:'222CSS',isDone:false},
+        {id:3,titleTask:'222REact',isDone:false},
+    ]
+
     return (
         <div className="App">
-           <Todolist title={'What to learn'}/>
-           <Todolist title={'What to buy'}/>
+           <Todolist title={'What to learn'} tasks={tasks1}/>
+           <Todolist title={'What to buy'} tasks={tasks2}/>
         </div>
     );
 }
