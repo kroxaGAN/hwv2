f4bde9b7623cbe9f28dfb7811fe1c03a01c94f44
Andrei Hutsau <mwplt@mail.ru>
2022-05-19 09:50:14 +0300

    make filtrathion button

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {Todolist} from "./Todolist";
 export type taskType={
     id:number,titleTask:string,isDone:boolean
 }
+export type filterType='all'|'active'|'completed'
 
 function App() {
     const [tasks, setTasks]=useState([
@@ -12,13 +13,27 @@ function App() {
         {id:2,titleTask:'CSS',isDone:false},
         {id:3,titleTask:'REact',isDone:true},
     ])
+    const [filter,setFilter]=useState<filterType>('all')
     const removeTask=(id:number)=>{
         setTasks(tasks.filter(el=>el.id!==id))
     }
+    let filteredTasks=tasks
+    if (filter==='active'){
+        filteredTasks=tasks.filter(el=>!el.isDone)
+    }
+    if (filter==='completed'){
+        filteredTasks=tasks.filter(el=>el.isDone)
+    }
+
 
     return (
         <div className="App">
-           <Todolist title={'What to learn'} tasks={tasks} removeTask={removeTask}/>
+           <Todolist
+               title={'What to learn'}
+               tasks={filteredTasks}
+               removeTask={removeTask}
+               setFilter={setFilter}
+           />
         </div>
     );
 }
